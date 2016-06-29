794b8c31d1d073c63935f2662c2e61c09e5535a2
F0RIS <F0RISW@gmail.com>
2016-06-29 21:41:34 +0300

    Plot Static Sample

--- app/src/main/java/bonlinetest/f0ris/com/b_onlinetest/Fragments/ChartFragment.java
+++ app/src/main/java/bonlinetest/f0ris/com/b_onlinetest/Fragments/ChartFragment.java
@@ -1,12 +1,24 @@
 package bonlinetest.f0ris.com.b_onlinetest.Fragments;
 
 
+import android.graphics.DashPathEffect;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.androidplot.util.PixelUtils;
+import com.androidplot.xy.CatmullRomInterpolator;
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.PointLabelFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeries;
+
+import java.util.Arrays;
+
+import bonlinetest.f0ris.com.b_onlinetest.AppController;
 import bonlinetest.f0ris.com.b_onlinetest.R;
 
 /**
@@ -14,6 +26,7 @@ import bonlinetest.f0ris.com.b_onlinetest.R;
  */
 public class ChartFragment extends Fragment {
 
+    private XYPlot plot;
 
     public ChartFragment() {
         // Required empty public constructor
@@ -23,7 +36,63 @@ public class ChartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chart, container, false);
+        View view = inflater.inflate(R.layout.fragment_chart, container, false);
+
+
+        // initialize our XYPlot reference:
+        plot = (XYPlot) view.findViewById(R.id.plot);
+
+        // create a couple arrays of y-values to plot:
+        Number[] series1Numbers = {1, 4, 2, 8, 4, 16, 8, 32, 16, 64};
+        Number[] series2Numbers = {5, 2, 10, 5, 20, 10, 40, 20, 80, 40};
+
+        // turn the above arrays into XYSeries':
+        // (Y_VALS_ONLY means use the element index as the x value)
+        XYSeries series1 = new SimpleXYSeries(Arrays.asList(series1Numbers),
+                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series1");
+
+        XYSeries series2 = new SimpleXYSeries(Arrays.asList(series2Numbers),
+                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2");
+
+        // create formatters to use for drawing a series using LineAndPointRenderer
+        // and configure them from xml:
+        LineAndPointFormatter series1Format = new LineAndPointFormatter();
+        series1Format.setPointLabelFormatter(new PointLabelFormatter());
+        series1Format.configure(AppController.getAppContext(),
+                R.xml.line_point_formatter_with_labels);
+
+        LineAndPointFormatter series2Format = new LineAndPointFormatter();
+        series2Format.setPointLabelFormatter(new PointLabelFormatter());
+        series2Format.configure(AppController.getAppContext(),
+                R.xml.line_point_formatter_with_labels_2);
+
+        // add an "dash" effect to the series2 line:
+        series2Format.getLinePaint().setPathEffect(
+                new DashPathEffect(new float[] {
+
+                        // always use DP when specifying pixel sizes, to keep things consistent across devices:
+                        PixelUtils.dpToPix(20),
+                        PixelUtils.dpToPix(15)}, 0));
+
+        // just for fun, add some smoothing to the lines:
+        // see: http://androidplot.com/smooth-curves-and-androidplot/
+        series1Format.setInterpolationParams(
+                new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
+
+        series2Format.setInterpolationParams(
+                new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
+
+        // add a new series' to the xyplot:
+        plot.addSeries(series1, series1Format);
+        plot.addSeries(series2, series2Format);
+
+        // reduce the number of range labels
+        plot.setTicksPerRangeLabel(3);
+
+        // rotate domain labels 45 degrees to make them more compact horizontally:
+        plot.getGraphWidget().setDomainLabelOrientation(-45);
+
+        return view;
     }
 
 }
